8dec568d4a65513be83588d9269c338d5160642d
Erokos <rkosanovic14@gmail.com>
2020-06-02 11:37:54 +0200

    Minor change
    Signed-off-by: Erokos <rkosanovic14@gmail.com>

--- eks/bastion-admin.sh
+++ eks/bastion-admin.sh
@@ -8,6 +8,6 @@ cni_link="$2"
 
 if [[ "${bastion_after_workers_ng}" != "true" ]]; then
   kubectl apply -f aws-auth-cm.yaml
-  # Apply the specified CNI version
+  # Apply the specified CNI version so nodes join the cluster
   kubectl apply -f ${cni_link}
 fi
